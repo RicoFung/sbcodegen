0153a5b171e458ed36bf6c645b26755cec033d35
RexFun <oleFun@icloud.com>
2017-12-06 15:29:22 +0800

    新增Action模板

---
+++ src/main/resources/template/${basepackage}/${subpackage}/action/${className}Action.java
@@ -0,0 +1,119 @@
+<#assign className = table.className>   
+<#assign classNameLower = className?uncap_first> 
+<#assign classNameLowerCase = table.classNameLowerCase>  
+<#assign classNameFirstLower = table.classNameFirstLower>  
+package ${basepackage}.${subpackage}.action;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import chok.devwork.BaseController;
+import chok.util.CollectionUtil;
+import ${basepackage}.${subpackage}.service.${className}Service;
+import ${basepackage}.${subpackage}.entity.${className};
+
+@Scope("prototype")
+@Controller
+@RequestMapping("/${subpackage}/${classNameLowerCase}")
+public class ${className}Action extends BaseController<${className}>
+{
+	@Autowired
+	private ${className}Service service;
+	
+	@RequestMapping("/add")
+	public String add() 
+	{
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/${classNameLowerCase}/add.jsp";
+	}
+	@RequestMapping("/add2")
+	public void add2(${className} po) 
+	{
+		try
+		{
+			service.add(po);
+			print("1");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			print("0:" + e.getMessage());
+		}
+	}
+	
+	@RequestMapping("/del")
+	public void del() 
+	{
+		try
+		{
+			service.del(CollectionUtil.toLongArray(req.getLongArray("id[]", 0l)));
+			result.setSuccess(true);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			result.setSuccess(false);
+			result.setMsg(e.getMessage());
+		}
+		printJson(result);
+	}
+	
+	@RequestMapping("/upd")
+	public String upd() 
+	{
+		put("po", service.get(req.getLong("id")));
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/${classNameLowerCase}/upd.jsp";
+	}
+	@RequestMapping("/upd2")
+	public void upd2(${className} po) 
+	{
+		try
+		{
+			service.upd(po);
+			print("1");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			print("0:" + e.getMessage());
+		}
+	}
+
+	@RequestMapping("/get")
+	public String get() 
+	{
+		put("po",service.get(req.getLong("id")));
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/${classNameLowerCase}/get.jsp";
+	}
+
+	@RequestMapping("/query")
+	public String query() 
+	{
+		put("queryParams",req.getParameterValueMap(false, true));
+		return "/admin/${classNameLowerCase}/query.jsp";
+	}
+	
+	@RequestMapping("/query2")
+	public void query2()
+	{
+		Map<String, Object> m = req.getParameterValueMap(false, true);
+		result.put("total",service.getCount(m));
+		result.put("rows",service.query(req.getDynamicSortMap(m)));
+		printJson(result.getData());
+	}
+	
+	@RequestMapping("/exp")
+	public void exp()
+	{
+		Map<String, Object> m = req.getParameterValueMap(false, true);
+		List<${className}> list = service.query(m);
+		exp(list, "xlsx");
+	}
+}
